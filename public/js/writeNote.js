737d1d6b6233ab2245f3d9911f9f28c4c380e5e1
TiloRC <tiloreneau@gmail.com>
2021-07-27 18:51:28 +0000

    can now add tags when writing messages

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -6,24 +6,52 @@ window.onload = (event) => {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
       googleUser = user;
+      handleTagAdding()
+
     } else {
       window.location = 'index.html'; // If not logged in, navigate back to login page.
     }
   });
 };
 
+const tags = []
+const handleTagAdding = () => {
+    const noteTags = document.querySelector('#noteTags')
+    const tagsDisplay = document.querySelector('#tagsDisplay')
+    noteTags.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13 && noteTags.value != "") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        console.log("enter key pressed within input")
+        // Make tags look nice
+        tagsDisplay.innerHTML += `      <span class="tag is-medium">${noteTags.value}</span>`
+        tags.push(noteTags.value)
+        noteTags.value = ""
+  }
+})
+}
+
+
+
 const handleNoteSubmit = () => {
   // 1. Capture the form data
   const noteTitle = document.querySelector('#noteTitle');
   const noteText = document.querySelector('#noteText');
+
+
+
+
   // 2. Format the data and write it to our database
   firebase.database().ref(`users/${googleUser.uid}`).push({
     title: noteTitle.value,
-    text: noteText.value
+    text: noteText.value,
+    tags: tags
   })
   // 3. Clear the form so that we can write a new note
   .then(() => {
     noteTitle.value = "";
     noteText.value = "";
+    document.querySelector('#tagsTagsDisplay').innerHTML = "";
+    tags = [];
   });
 }
